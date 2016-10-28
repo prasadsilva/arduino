41e1c42836adea11a6cf2b55e755a2ec6b727ebe
Prasad Silva <prasad.silva@gmail.com>
2016-10-28 01:02:25 -0400

    Tweaked rainbow cycle to have a trail effect

--- neopixel/gemma_neopixel16ring_rainbow_cycle/gemma_neopixel16ring_rainbow_cycle.ino
+++ neopixel/gemma_neopixel16ring_rainbow_cycle/gemma_neopixel16ring_rainbow_cycle.ino
@@ -9,87 +9,65 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, OUTPUT_PIN);
 
 uint8_t brightness = 255;
 uint8_t hue = 0, saturation = 255, value = 255;
-uint8_t red, green, blue;
 
 // Modified HSV->RGB function
 // Original: http://stackoverflow.com/questions/3018313/algorithm-to-convert-rgb-to-hsv-and-hsv-to-rgb-in-range-0-255-for-both
 //
-void ComputeNextRgb()
+uint32_t ComputeColor(uint8_t hparam, uint8_t sparam, uint8_t vparam)
 {  
-    uint8_t region, p, q, t;
-    uint32_t h, s, v, remainder;
+  uint8_t region, p, q, t;
+  uint32_t h, s, v, remainder;
 
-    // converting to 16 bit to prevent overflow
-    h = hue;
-    s = saturation;
-    v = value;
-    
-    if (hue == 255) hue = 0;
-    else hue++;
+  // Using 32 bit to support bit shift later
+  h = hparam;
+  s = sparam;
+  v = vparam;
 
-    if (saturation == 0)
-    {
-        red = value;
-        green = value;
-        blue = value;
-        return;
-    }
+  if (saturation == 0)
+  {
+    return pixels.Color(value, value, value);
+  }
 
-    region = h / 43;
-    remainder = (h - (region * 43)) * 6; 
+  region = h / 43;
+  remainder = (h - (region * 43)) * 6; 
 
-    p = (v * (255 - s)) >> 8;
-    q = (v * (255 - ((s * remainder) >> 8))) >> 8;
-    t = (v * (255 - ((s * (255 - remainder)) >> 8))) >> 8;
+  p = (v * (255 - s)) >> 8;
+  q = (v * (255 - ((s * remainder) >> 8))) >> 8;
+  t = (v * (255 - ((s * (255 - remainder)) >> 8))) >> 8;
 
-    switch (region)
-    {
-        case 0:
-            red = v;
-            green = t;
-            blue = p;
-            break;
-        case 1:
-            red = q;
-            green = v;
-            blue = p;
-            break;
-        case 2:
-            red = p;
-            green = v;
-            blue = t;
-            break;
-        case 3:
-            red = p;
-            green = q;
-            blue = v;
-            break;
-        case 4:
-            red = t;
-            green = p;
-            blue = v;
-            break;
-        default:
-            red = v;
-            green = p;
-            blue = q;
-            break;
-    }
+  switch (region)
+  {
+    case 0:
+        return pixels.Color(v, t, p);
+    case 1:
+        return pixels.Color(q, v, p);
+    case 2:
+        return pixels.Color(p, v, t);    
+    case 3:
+        return pixels.Color(p, q, v);    
+    case 4:
+        return pixels.Color(t, p, v);    
+    default:
+        return pixels.Color(v, p, q);
+  }
 }
 
 void setup() 
 {
   pixels.begin();
+  pixels.setBrightness(brightness);
+  pixels.show();
 }
 
 void loop() 
-{
-  ComputeNextRgb();
-  
-  pixels.setBrightness(brightness);
+{ 
   for (uint8_t ledNumber = 0; ledNumber < NUM_PIXELS; ledNumber++) {
-    pixels.setPixelColor(ledNumber, red, green, blue);
+    pixels.setPixelColor(ledNumber, ComputeColor(hue, saturation, value / ledNumber);
   }
   pixels.show();
   delay(200);
+  
+  if (hue++) {
+    hue = 0;
+  }
 }
