869880b354aec6fbc072baa355acc94c69cae978
Prasad Silva <prasad.silva@gmail.com>
2016-10-28 16:04:01 -0400

    Fix for rainbow cycle

--- neopixel/gemma_neopixel16ring_rainbow_cycle/gemma_neopixel16ring_rainbow_cycle.ino
+++ neopixel/gemma_neopixel16ring_rainbow_cycle/gemma_neopixel16ring_rainbow_cycle.ino
@@ -8,24 +8,23 @@ const uint8_t   NUM_PIXELS   = 16;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, OUTPUT_PIN);
 
 uint8_t brightness = 255;
-uint8_t hue = 0, saturation = 255, value = 255;
+uint32_t hue = 0, saturation = 255;
+
+const uint8_t CYCLE_DELAY = 50;
+
+uint8_t pixel_shift = 0;
 
 // Modified HSV->RGB function
 // Original: http://stackoverflow.com/questions/3018313/algorithm-to-convert-rgb-to-hsv-and-hsv-to-rgb-in-range-0-255-for-both
 //
-uint32_t ComputeColor(uint8_t hparam, uint8_t sparam, uint8_t vparam)
+uint32_t ComputeColor(uint32_t h, uint32_t s, uint32_t v)
 {  
   uint8_t region, p, q, t;
-  uint32_t h, s, v, remainder;
-
-  // Using 32 bit to support bit shift later
-  h = hparam;
-  s = sparam;
-  v = vparam;
+  uint32_t remainder;
 
   if (saturation == 0)
   {
-    return pixels.Color(value, value, value);
+    return pixels.Color(v, v, v);
   }
 
   region = h / 43;
@@ -61,13 +60,22 @@ void setup()
 
 void loop() 
 { 
-  for (uint8_t ledNumber = 0; ledNumber < NUM_PIXELS; ledNumber++) {
-    pixels.setPixelColor(ledNumber, ComputeColor(hue, saturation, value / ledNumber);
+  uint8_t curr_pixel = pixel_shift;
+  for (uint8_t i = 0; i < NUM_PIXELS; i++) {
+    pixels.setPixelColor(curr_pixel, ComputeColor(hue, saturation, (i * 16) + 8));
+    if (curr_pixel++ > NUM_PIXELS) {
+      curr_pixel = 0;
+    }
   }
   pixels.show();
-  delay(200);
   
-  if (hue++) {
+  if (pixel_shift++ > NUM_PIXELS) {
+    pixel_shift = 0;
+  }
+  
+  if (hue++ > 256) {
     hue = 0;
   }
+  
+  delay(CYCLE_DELAY);
 }
